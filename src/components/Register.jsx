955e46c441415d2d96fbd21e817a286e4cd19dce
matiasfzuniga <matiaszuniga93@gmail.com>
2021-06-30 14:50:13 -0300

    vista de registro y login

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,89 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useForm } from "../hooks/useForm";
+import {useHistory } from "react-router-dom";
+import validator from 'validator'
 
-function Register() {
-    return (
-        <div>
-            <div>Register</div>
+
+function  Register  ()  {
+const history = useHistory()
+  const [formRegisterValues, handleInputChange] = useForm({
+    Name: "",
+    Email:"",
+    Password:""
+  });
+  const { Name ,Email, Password } = formRegisterValues;
+
+  const [emailMsg, setEmailMsg] = useState("")
+  
+
+  const validateEmail = (e) => {
+      var email = e.target.value
+
+      if (validator.isEmail(email)){
+        setEmailMsg('Valid Email!')
+      } else {
+        setEmailMsg('enter valid Email!')
+      }
+    handleInputChange(e)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    console.log('submit')
+   
+    history.push("/login")
+  };
+
+  return (
+    <div className="registerBody">
+      <form className="registerForm" onSubmit={handleSubmit}>
+      <div className='formContainer'>
+        <div className="welcome-text">Welcome !</div>
+
+        <div className="createAcc">Register</div>
+
+        <div className="emailInput">
+          <input
+            type="text"
+            name="Name"
+            placeholder="Name"
+            value={Name}
+            onChange={handleInputChange}
+            required
+          />
         </div>
-    )
-}
 
-export default Register
+        <div className="emailInput">
+          <input
+            type="text"
+            name="Email"
+            placeholder="Email"
+            value={Email}
+            onChange={ (e) => validateEmail(e)}
+           
+            required
+          />
+          <br />
+          <span>{emailMsg}</span>
+        </div>
+        <div className="passInput">
+        <input
+            type="password"
+            name="Password"
+            placeholder="Password"
+            value={Password}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <button type="submit" className="registerBtn" >
+          Register
+        </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import NotFound from "../components/NotFound";
-import "../assets/styles/App.scss"
 
+import "../assets/styles/App.scss"
+import NotFound from "../components/NotFound";
+import Header from "../components/Header";
+import Register from "../components/Register"
+import LogIn from "../components/LogIn"
+import Layout from "../components/Layout";
 const App = () => {
   return (
     <BrowserRouter>
+        
+        <Layout>
         <Switch>
-          <Route exact path="/" component={NotFound} />
+          <Route exact path="/" component={NotFound} />  
+          <Route exact path ="/login" component={LogIn} />
+          <Route exact path ="/register" component={Register} />
         </Switch>
+        </Layout>
     </BrowserRouter>
   )
 };
