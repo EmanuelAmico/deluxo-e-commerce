bf7bfbb8e2a2521e5b7c1ad2639b4d3cc7f1ed47
InevitableDesire <emanuelamicoo@gmail.com>
2021-07-02 02:38:27 -0300

    persistencia del login y fix async/await

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { setUser } from "../redux/users";
 
 function Header() {
+  const dispatch = useDispatch()
+
+  useEffect(async ()=> {
+    try {
+      if(localStorage.getItem('token')){
+        const token = localStorage.getItem('token')
+        const res = await axios.get('/api/users/me', {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        const user = res.data
+        dispatch(setUser({...user, token, isLoggedIn: true}))
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
+
   return (
     <header>
       <div className="links">
