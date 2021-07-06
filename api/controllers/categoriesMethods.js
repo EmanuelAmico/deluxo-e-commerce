d601d2d66782e23717298fbba050c45dd4bdebb4
InevitableDesire <emanuelamicoo@gmail.com>
2021-07-06 19:29:21 -0300

    get route implemented

--- api/controllers/categoriesMethods.js
+++ api/controllers/categoriesMethods.js
@@ -2,8 +2,21 @@ const { Categories } = require("../models");
 
 const getCategories = async (req, res, next) => {
   try {
-    const categories = await Categories.findAll()
-    res.status(200).send(categories)
+    const categories = await Categories.findAll({
+      attributes: {
+        exclude: ['id', 'productId']
+      }
+    })
+    const filteredCategories = []
+    categories.forEach(({name}) => {
+      console.log(name)
+      if(!filteredCategories.includes(name))
+        filteredCategories.push(name)
+    })
+    console.log(filteredCategories) // ['Hoodie', 'Jeans', etc]
+    /* const finalCategories = filteredCategories.map(category => ({ name: category })) */
+    const finalCategories = { categories: filteredCategories }
+    res.status(200).send(finalCategories)
   } catch (error) {
     next(error)
   }
