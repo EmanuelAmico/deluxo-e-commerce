ed86398df29cff402f88352a254c77456864cae8
Alex Emanuel Amico <emanuelamicoo@gmail.com>
2021-08-26 21:02:20 +0000

    logger for requests

--- back-end/server.js
+++ back-end/server.js
@@ -2,17 +2,23 @@ const express = require("express");
 const morgan = require("morgan");
 const https = require("https");
 const fs = require("fs");
+const path = require("path")
 const db = require("./db");
 const routes = require("./routes");
 const cors = require("cors")
 const app = express();
 
 // Configs
-app.use(morgan("dev"));
+app.use(morgan("short"));
 app.use(express.json()); //Para los GET no hace falta pero para los POST sí :)
 app.use(express.urlencoded({ extended: false })); //Para que funcionen los formularios del front
 app.use(cors())
 app.options('/', cors()) // Para que funcionen los méotodos OPTIONS en el cross-origin
+
+// Logging
+app.use(morgan('combined', {
+  stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+}))
 
 // Https
 const key = fs.readFileSync(__dirname + "/certs/privkey.pem");
